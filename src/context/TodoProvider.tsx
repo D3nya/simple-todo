4cc0b519a47a1c17fe04f177aa70d76326c597b2
Denis <51379629+D3nya@users.noreply.github.com>
2025-06-03 10:00:40 +0300

    some fixes

--- src/context/TodoProvider.tsx
+++ src/context/TodoProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import { TodoContext } from "./TodoContext";
 import { initialState, todoReducer } from "./todoReducer";
 
@@ -6,8 +6,22 @@ interface TodoProviderProps {
   children: React.ReactNode;
 }
 
+const STORAGE_KEY = "todoAppState";
+
 export const TodoProvider: React.FC<TodoProviderProps> = ({ children }) => {
-  const [state, dispatch] = useReducer(todoReducer, initialState);
+  const [state, dispatch] = useReducer(todoReducer, initialState, (initial) => {
+    const savedState = localStorage.getItem(STORAGE_KEY);
+    return savedState ? JSON.parse(savedState) : initial;
+  });
+
+  // Save state
+  useEffect(() => {
+    try {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+    } catch (e) {
+      console.error("Error:", e);
+    }
+  }, [state]);
 
   return <TodoContext.Provider value={{ state, dispatch }}>{children}</TodoContext.Provider>;
 };
